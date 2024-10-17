b431ac4375ad413c729d1648967402959a8f05e5
Clarence21-ai <agtualclarence16@gmail.com>
2024-10-17 15:22:29 +0000

    Initial commit with app.js

---
+++ objectActivty.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Sedan",
+    model: "Corolla",
+    color: "Blue"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
